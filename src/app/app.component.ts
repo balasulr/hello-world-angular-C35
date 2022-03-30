28d7d1ec9500d72693b0ef2543522e000e202d26
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-03-30 16:37:54 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ // Decorator called Component
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hello-world-angular';
+export class AppComponent { // Class
+  title = 'The bootcamp';
+  maxOn: boolean = true;
+  names: string[] = [ "Ann", "Bob", "Chris", "David", "Ellen", "Fred"];
+  customers: any[] = [ // any means that can be any type
+    { id: 1, name : "MAX", sales: 10000, stateCode: "OH"},
+    { id: 2, name : "PG", sales: 100000, stateCode: "OH"},
+    { id: 3, name : "Great American", sales: 1000000, stateCode: "OH"}
+  ];
+
+  aString1: string = "";
+  aString2: string = "";
+
+  copy(): void {
+    this.aString2 = this.aString1;
+  }
+
+  clicked(): void {
+    if(this.maxOn) {
+      this.title = "The MAXimum boot camp";
+      this.maxOn = !this.maxOn;
+    } else {
+      this.maxOn = !this.maxOn;
+      this.title = "The boot camp"
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Required when want to do two way binding in app.module.ts
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; // Class where ts is assumed
 
-@NgModule({
+@NgModule({ // Decorator
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } // Class
